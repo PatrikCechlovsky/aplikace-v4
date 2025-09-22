bf9df7f7738b6822c880c8ef68e26b6fb740c6df
PatrikCechlovsky <patrik.cechlovsky@centrum.cz>
2025-09-22 13:47:27 +0200

    Implement renderModule for finance demo display

--- src/modules/090-finance/index.js
+++ src/modules/090-finance/index.js
@@ -1,2 +1,4 @@
-// Vstupní bod modulu Finance
-module.exports = {};
+export async function renderModule(root, { kind, id }) {
+  const c = (h)=>`<div class="card p-4">${h}</div>`
+  return root.innerHTML = c('💸 Finance – demo přehled.')
+}
