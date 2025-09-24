5aff15e12eef071bcd0eae57fc1365fa9a2cf17d
PatrikCechlovsky <patrik.cechlovsky@centrum.cz>
2025-09-24 13:57:51 +0200

    Change SUPABASE_ANON_KEY to a new token
    Updated SUPABASE_ANON_KEY with a new value.

--- src/supabase.js
+++ src/supabase.js
@@ -2,7 +2,7 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 export const SUPABASE_URL = "https://jgqgfbuagoypladyjjfl.supabase.co";
-export const SUPABASE_ANON_KEY = "xxx"; // sem vlož celý anon public key
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpncWdmYnVhZ295cGxhZHlqamZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg1MTA3MTcsImV4cCI6MjA3NDA4NjcxN30.O5vvOyiYlIJb_bxaBbYF8Sn9sm2ReB7vDgASVm3dT3g"; // sem vlož celý anon public key
 
 // jediný klient pro celou aplikaci
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
