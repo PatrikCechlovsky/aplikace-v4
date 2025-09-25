255244785ffb176b81ffaf90aa60d33fdb36a57d
PatrikCechlovsky <patrik.cechlovsky@centrum.cz>
2025-09-25 10:56:58 +0200

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 // src/supabase.js
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
-window.supabase = supabase; // zpřístupní klienta v konzoli i mimo modul
+
 
 export const SUPABASE_URL = "https://jgqgfbuagoypladyjjfl.supabase.co";
 export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpncWdmYnVhZ295cGxhZHlqamZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg1MTA3MTcsImV4cCI6MjA3NDA4NjcxN30.O5vvOyiYlIJb_bxaBbYF8Sn9sm2ReB7vDgASVm3dT3g";
@@ -13,6 +13,8 @@ export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
     detectSessionInUrl: true,
   },
 });
+
+window.supabase = supabase; // zpřístupní klienta v konzoli i mimo modul
 
 // log jen pro kontrolu
 console.log('[SUPABASE INIT]', SUPABASE_URL, SUPABASE_ANON_KEY.slice(0,8)+'…');
